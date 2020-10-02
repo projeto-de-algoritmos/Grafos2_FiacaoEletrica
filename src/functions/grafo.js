675ada53702b5c037a4760e3f8a57a4e1bc815b7
Andre Eduardo <andre.eduardo11@hotmail.com>
2020-10-02 03:10:54 -0300

    monta grafo

--- src/functions/grafo.js
+++ src/functions/grafo.js
@@ -1,7 +1,9 @@
-class Grafo {
-
+import Heap from './fila_de_prioridades';
+export default class Grafo {
   constructor() {
     this.vertices = {};
+    this.custo = 0;
+    this.caminho = {};
   }
 
   // adição de um vertice 'id' no grafo
@@ -18,29 +20,31 @@ class Grafo {
   // adicionar aresta aos vertices * parametro 'tipo' opcional (se nao for passado ele considera uma aresta orientada (a->c))
   // se for passado o valor 'nao_orientado' em 'tipo', ele alem de fazer a aresta (a->c) ele faz a aresta (c->a)
   adcAresta(origem, destino, peso, tipo) {
-
-    if(typeof(tipo) === 'undefined' || tipo === 'orientado' || tipo === 'nao_orientado') {
+    if (
+      typeof tipo === 'undefined' ||
+      tipo === 'orientado' ||
+      tipo === 'nao_orientado'
+    ) {
       let nova_aresta = {
         origem: origem,
         destino: destino,
         peso: peso,
-      }
+      };
       this.vertices[origem]['vizinhos'].push(nova_aresta);
       this.vertices[origem]['grau']++;
     }
-    if(tipo === 'nao_orientado') {
+    if (tipo === 'nao_orientado') {
       let nova_aresta_invertida = {
         origem: destino,
         destino: origem,
         peso: peso,
-      }
+      };
 
       this.vertices[destino]['vizinhos'].push(nova_aresta_invertida);
       this.vertices[destino]['grau']++;
     }
-
   }
-  
+
   obterVertices() {
     return this.vertices; // retornando todos os nós do grafo
   }
@@ -53,7 +57,7 @@ class Grafo {
         let nova_aresta = {
           de: vertice,
           para: aresta.toString(),
-        }
+        };
         arestas.push(nova_aresta);
       }
     }
@@ -61,10 +65,9 @@ class Grafo {
     return arestas;
   }
 
-// tentar implementar a versao otima do algoritmo de prim
+  // tentar implementar a versao otima do algoritmo de prim
 
   prim(inicial) {
-
     const heap = new Heap();
     let vertices_explorados = {};
     let arestas = [];
@@ -72,99 +75,108 @@ class Grafo {
 
     // adicionando todos os vertices a fila de prioridades (heap)
     // custo O(n)
-    for(let vertice in this.vertices) {
+    for (let vertice in this.vertices) {
       heap.inserir(vertice);
-    }    
+    }
     //
     //função 'atualizar_prioridades' da classe heap leva o elemento (se for o de menor valor) ao topo do heap
     heap.atualizar_prioridades(inicial, 0); // operação de custo (O log N)
     //
     // remove o elemento de indice 0 da fila de prioridades ou head do heap.
-    let u = heap.remover_o_primeiro();   // operacao de custo O(log N) 
-    
-    while(heap.fila_de_prioridades.length !== 0) {
-      
+    let u = heap.remover_o_primeiro(); // operacao de custo O(log N)
+
+    while (heap.fila_de_prioridades.length !== 0) {
       vertices_explorados[u] = u; // adiciona o primeiro elemento que foi removido aos vertices já explorados (CUT)
-     
-      for(let vizinho of this.vertices[u]['vizinhos']) { // para cada vizinho de u
+
+      for (let vizinho of this.vertices[u]['vizinhos']) {
+        // para cada vizinho de u
         // o if abaixo verifica se o vizinho de u em questao esta fora do cut (vertices visitados)
         // verifica tambem se o custo do vizinho atual é menor do que o custo que esta atrelado a ele na fila de prioridades
-        if(typeof(vertices_explorados[vizinho['destino']]) === 'undefined' && vizinho['peso'] < heap.fila_de_prioridades[heap.estrutura_auxiliar[vizinho['destino']]['posicao']]['custo']) {
+        if (
+          typeof vertices_explorados[vizinho['destino']] === 'undefined' &&
+          vizinho['peso'] <
+            heap.fila_de_prioridades[
+              heap.estrutura_auxiliar[vizinho['destino']]['posicao']
+            ]['custo']
+        ) {
           // se as condicoes acima forem verdade entao atualiza a posição desse vizinho na fila de prioridades (heap)
-          heap.atualizar_prioridades(vizinho['destino'], vizinho['peso'], u) // operação de custo O(log N)
+          heap.atualizar_prioridades(vizinho['destino'], vizinho['peso'], u); // operação de custo O(log N)
           // o parametro U passado na função acima, salva o elemento de onde aquele nó partiu.
-        }    
+        }
       }
       // após visitar cada vizinho de u, o algoritmo irá adicionar uma nova aresta à árvore geradora.
-  
+
       let nova_aresta = {
         origem: heap.fila_de_prioridades[0]['origem'],
         destino: heap.fila_de_prioridades[0]['id'],
         custo: heap.fila_de_prioridades[0]['custo'],
-      }
+      };
 
       arestas.push(nova_aresta); // adicionando a aresta a estrutura que armazena as arestas
       total += heap.fila_de_prioridades[0]['custo']; // adicionando o custo daquela aresta ao custo total da arvore geradora
       u = heap.remover_o_primeiro(); // setar o novo u;
-      
-    }    
+    }
+    this.custo = total;
+    this.caminho = arestas;
     console.log(arestas);
     console.log(total);
-   
   }
-
+  getCaminho() {
+    return this.caminho;
+  }
+  getCusto() {
+    return this.custo;
+  }
   imprimir() {
     console.log(this.vertices);
   }
 }
 
+// const graph = new Grafo();
 
-const graph = new Grafo();
+// // dados abaixo vindos do slide do professor
 
+// var a = 'A';
+// var b = 'B';
+// var c = 'C';
+// var d = 'D';
+// var e = 'E';
+// var f = 'F';
+// var g = 'G';
+// var h = 'H';
 
-// dados abaixo vindos do slide do professor
+// graph.adcVertice(a);
+// graph.adcVertice(b);
+// graph.adcVertice(c);
+// graph.adcVertice(d);
+// graph.adcVertice(e);
+// graph.adcVertice(f);
+// graph.adcVertice(g);
+// graph.adcVertice(h);
 
-var a = 'A';
-var b = 'B';
-var c = 'C';
-var d = 'D';
-var e = 'E';
-var f = 'F';
-var g = 'G';
-var h = 'H';
-
-graph.adcVertice(a);
-graph.adcVertice(b);
-graph.adcVertice(c);
-graph.adcVertice(d);
-graph.adcVertice(e);
-graph.adcVertice(f);
-graph.adcVertice(g);
-graph.adcVertice(h);
-
-// exemplo de grafo 1 da videoaula do professor
-graph.adcAresta('A', 'B', 4, 'nao_orientado');
-graph.adcAresta('A', 'C', 6, 'nao_orientado');
-graph.adcAresta('A', 'D', 16, 'nao_orientado');
-graph.adcAresta('B', 'G', 24, 'nao_orientado');
-graph.adcAresta('C', 'E', 5, 'nao_orientado');
-graph.adcAresta('C', 'D', 8, 'nao_orientado');
-graph.adcAresta('C', 'G', 23, 'nao_orientado');
-graph.adcAresta('D', 'E', 10, 'nao_orientado');
-graph.adcAresta('D', 'H', 21, 'nao_orientado');
-graph.adcAresta('E', 'G', 18, 'nao_orientado');
-graph.adcAresta('E', 'F', 11, 'nao_orientado');
-graph.adcAresta('E', 'H', 14, 'nao_orientado');
-graph.adcAresta('F', 'G', 9, 'nao_orientado');
-graph.adcAresta('F', 'H', 7, 'nao_orientado');
-// exemplo de grafo 2 da videoaula do professor
-// graph.adcAresta('A', 'B', 2, 'nao_orientado');
-// graph.adcAresta('A', 'C', 3, 'nao_orientado');
-// graph.adcAresta('B', 'C', 5, 'nao_orientado');
-// graph.adcAresta('B', 'D', 4, 'nao_orientado');
-// graph.adcAresta('B', 'E', 4, 'nao_orientado');
+// // exemplo de grafo 1 da videoaula do professor
+// graph.adcAresta('A', 'B', 4, 'nao_orientado');
+// graph.adcAresta('A', 'C', 6, 'nao_orientado');
+// graph.adcAresta('A', 'D', 16, 'nao_orientado');
+// graph.adcAresta('B', 'G', 24, 'nao_orientado');
 // graph.adcAresta('C', 'E', 5, 'nao_orientado');
-// graph.adcAresta('D', 'E', 2, 'nao_orientado');
-// graph.adcAresta('D', 'F', 3, 'nao_orientado');
-// graph.adcAresta('E', 'F', 5, 'nao_orientado');
-graph.prim('E')
+// graph.adcAresta('C', 'D', 8, 'nao_orientado');
+// graph.adcAresta('C', 'G', 23, 'nao_orientado');
+// graph.adcAresta('D', 'E', 10, 'nao_orientado');
+// graph.adcAresta('D', 'H', 21, 'nao_orientado');
+// graph.adcAresta('E', 'G', 18, 'nao_orientado');
+// graph.adcAresta('E', 'F', 11, 'nao_orientado');
+// graph.adcAresta('E', 'H', 14, 'nao_orientado');
+// graph.adcAresta('F', 'G', 9, 'nao_orientado');
+// graph.adcAresta('F', 'H', 7, 'nao_orientado');
+// // exemplo de grafo 2 da videoaula do professor
+// // graph.adcAresta('A', 'B', 2, 'nao_orientado');
+// // graph.adcAresta('A', 'C', 3, 'nao_orientado');
+// // graph.adcAresta('B', 'C', 5, 'nao_orientado');
+// // graph.adcAresta('B', 'D', 4, 'nao_orientado');
+// // graph.adcAresta('B', 'E', 4, 'nao_orientado');
+// // graph.adcAresta('C', 'E', 5, 'nao_orientado');
+// // graph.adcAresta('D', 'E', 2, 'nao_orientado');
+// // graph.adcAresta('D', 'F', 3, 'nao_orientado');
+// // graph.adcAresta('E', 'F', 5, 'nao_orientado');
+// graph.prim('E')
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,27 +1,95 @@
 import React, { useState, useEffect } from 'react';
 import { Form, Input } from '@rocketseat/unform';
-import Graph from 'react-graph-vis';
+//import Graph from 'react-graph-vis';
+import Graph from 'vis-react';
 import { Container, Menu } from './styles';
 
-function Dashboard(props) {
+import Grafo from '../../functions/grafo';
+
+function Dashboard() {
+  const grafo = new Grafo();
+
+  // dados abaixo vindos do slide do professor
+
+  // var a = 'A';
+  // var b = 'B';
+  // var c = 'C';
+  // var d = 'D';
+  // var e = 'E';
+  // var f = 'F';
+  // var g = 'G';
+  // var h = 'H';
+
+  // grafo.adcVertice('A');
+  // grafo.adcVertice(b);
+  // grafo.adcVertice(c);
+  // grafo.adcVertice(d);
+  // grafo.adcVertice(e);
+  // grafo.adcVertice(f);
+  // grafo.adcVertice(g);
+  // grafo.adcVertice(h);
+
+  // // exemplo de grafo 1 da videoaula do professor
+  // grafo.adcAresta('A', 'B', 4, 'nao_orientado');
+  // grafo.adcAresta('A', 'C', 6, 'nao_orientado');
+  // grafo.adcAresta('A', 'D', 16, 'nao_orientado');
+  // grafo.adcAresta('B', 'G', 24, 'nao_orientado');
+  // grafo.adcAresta('C', 'E', 5, 'nao_orientado');
+  // grafo.adcAresta('C', 'D', 8, 'nao_orientado');
+  // grafo.adcAresta('C', 'G', 23, 'nao_orientado');
+  // grafo.adcAresta('D', 'E', 10, 'nao_orientado');
+  // grafo.adcAresta('D', 'H', 21, 'nao_orientado');
+  // grafo.adcAresta('E', 'G', 18, 'nao_orientado');
+  // grafo.adcAresta('E', 'F', 11, 'nao_orientado');
+  // grafo.adcAresta('E', 'H', 14, 'nao_orientado');
+  // grafo.adcAresta('F', 'G', 9, 'nao_orientado');
+  // grafo.adcAresta('F', 'H', 7, 'nao_orientado');
+  // // exemplo de grafo 2 da videoaula do professor
+  // // grafo.adcAresta('A', 'B', 2, 'nao_orientado');
+  // // grafo.adcAresta('A', 'C', 3, 'nao_orientado');
+  // // grafo.adcAresta('B', 'C', 5, 'nao_orientado');
+  // // grafo.adcAresta('B', 'D', 4, 'nao_orientado');
+  // // grafo.adcAresta('B', 'E', 4, 'nao_orientado');
+  // // grafo.adcAresta('C', 'E', 5, 'nao_orientado');
+  // // grafo.adcAresta('D', 'E', 2, 'nao_orientado');
+  // // grafo.adcAresta('D', 'F', 3, 'nao_orientado');
+  // // grafo.adcAresta('E', 'F', 5, 'nao_orientado');
+  // grafo.prim('E');
+
   const [nodeSelect, setNodeSelect] = useState();
+  const [custo, setCusto] = useState(0);
   const [edge, setEdge] = useState([]);
+  const [selectEdge, setSelectEdge] = useState([]);
   const [graph, setgraph] = useState({
     // inicializa os nós e arestas do grafo
-    nodes: [
-      { id: 1, label: 'Node 1', title: 'node 1 tootip text' },
-      { id: 2, label: 'Node 2', title: 'node 2 tootip text' },
-      { id: 3, label: 'Node 3', title: 'node 3 tootip text' },
-      { id: 4, label: 'Node 4', title: 'node 4 tootip text' },
-      { id: 5, label: 'Node 5', title: 'node 5 tootip text' },
-    ],
-    edges: [
-      { from: 1, to: 2 },
-      { from: 1, to: 3 },
-      { from: 2, to: 4 },
-      { from: 2, to: 5 },
-    ],
+    nodes: [],
+    edges: [],
   });
+
+  useEffect(() => {
+    var node = [];
+    var edge = [];
+    var tamanho = 10; // numero de nós
+    for (var i = 0; i < tamanho; i++) {
+      var to = Aleatorio(1, tamanho - 1);
+      var value = Aleatorio(1, 100);
+      node.push({ id: i, label: `Casa ${i}` });
+      //grafo.adcVertice(`${i}`);
+
+      edge.push({
+        from: i,
+        to: to,
+        label: `${value}`,
+      });
+
+      //grafo.adcAresta(`${i}`, `${to}`, value);
+    }
+    setEdge(edge);
+    setgraph({
+      nodes: node,
+      edges: edge,
+    });
+  }, []);
 
   const options = {
     // define as opçoes para renderizaçao do grafo
@@ -46,13 +114,60 @@ function Dashboard(props) {
   const events = {
     // função que captura os nós selecionados pelo usuario
     select: function (event) {
-      var { nodes } = event;
+      var { nodes, edges } = event;
       setNodeSelect(nodes);
+      setSelectEdge(edges);
     },
   };
-  function CreateGraph() {
-    console.log(nodeSelect);
+
+  function Aleatorio(min, max) {
+    // gera valores aleatorios
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
+  function CreateGraph() {
+    var n = [];
+    var e = [];
+    nodeSelect.map((no) => {
+      console.log(no);
+      grafo.adcVertice(`${no}`);
+      n.push({ id: no, label: `Casa ${no}` });
+    });
+    edge.map((ed) => {
+      var len = 0;
+      //grafo.adcVertice(`${no}`);
+
+      while (len < selectEdge.length) {
+        if (selectEdge[len] == ed['id']) {
+          console.log(ed);
+          grafo.adcAresta(
+            `${ed['from']}`,
+            `${ed['to']}`,
+            parseInt(ed['label'], 10)
+          );
+        }
+        len++;
+      }
+    });
+    grafo.prim(`${nodeSelect[0]}`);
+    var caminho = grafo.getCaminho();
+    caminho.map((Cami) => {
+      e.push({
+        from: Cami['origem'],
+        to: Cami['destino'],
+        label: `${Cami['custo']}`,
+      });
+    });
+
+    setgraph({
+      nodes: n,
+      edges: e,
+    });
+    console.log(grafo.getCusto());
+    setCusto(grafo.getCusto());
+  }
+
   return (
     <Container>
       <Menu>
@@ -69,8 +184,8 @@ function Dashboard(props) {
         </Form>
         <div>
           <p>
-            Para percorrer o caminho selcionado , da melhor forma possivel, você
-            deve perccorer "X" km
+            {`Para percorrer o caminho selcionado , da melhor forma possivel, você
+            deve perccorer "${custo}" km`}
           </p>
         </div>
       </Menu>
