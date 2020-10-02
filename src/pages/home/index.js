c39293a79527b4bfcdeec666dcd17e194d238934
Andre Eduardo <andre.eduardo11@hotmail.com>
2020-10-02 17:03:52 -0300

    corrige erros na arvore geradora minima

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,61 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Form, Input } from '@rocketseat/unform';
-//import Graph from 'react-graph-vis';
-import Graph from 'vis-react';
+import Graph from 'react-graph-vis';
 import { Container, Menu } from './styles';
 
 import Grafo from '../../functions/grafo';
 
 function Dashboard() {
-  const grafo = new Grafo();
-
-  // dados abaixo vindos do slide do professor
-
-  // var a = 'A';
-  // var b = 'B';
-  // var c = 'C';
-  // var d = 'D';
-  // var e = 'E';
-  // var f = 'F';
-  // var g = 'G';
-  // var h = 'H';
-
-  // grafo.adcVertice('A');
-  // grafo.adcVertice(b);
-  // grafo.adcVertice(c);
-  // grafo.adcVertice(d);
-  // grafo.adcVertice(e);
-  // grafo.adcVertice(f);
-  // grafo.adcVertice(g);
-  // grafo.adcVertice(h);
-
-  // // exemplo de grafo 1 da videoaula do professor
-  // grafo.adcAresta('A', 'B', 4, 'nao_orientado');
-  // grafo.adcAresta('A', 'C', 6, 'nao_orientado');
-  // grafo.adcAresta('A', 'D', 16, 'nao_orientado');
-  // grafo.adcAresta('B', 'G', 24, 'nao_orientado');
-  // grafo.adcAresta('C', 'E', 5, 'nao_orientado');
-  // grafo.adcAresta('C', 'D', 8, 'nao_orientado');
-  // grafo.adcAresta('C', 'G', 23, 'nao_orientado');
-  // grafo.adcAresta('D', 'E', 10, 'nao_orientado');
-  // grafo.adcAresta('D', 'H', 21, 'nao_orientado');
-  // grafo.adcAresta('E', 'G', 18, 'nao_orientado');
-  // grafo.adcAresta('E', 'F', 11, 'nao_orientado');
-  // grafo.adcAresta('E', 'H', 14, 'nao_orientado');
-  // grafo.adcAresta('F', 'G', 9, 'nao_orientado');
-  // grafo.adcAresta('F', 'H', 7, 'nao_orientado');
-  // // exemplo de grafo 2 da videoaula do professor
-  // // grafo.adcAresta('A', 'B', 2, 'nao_orientado');
-  // // grafo.adcAresta('A', 'C', 3, 'nao_orientado');
-  // // grafo.adcAresta('B', 'C', 5, 'nao_orientado');
-  // // grafo.adcAresta('B', 'D', 4, 'nao_orientado');
-  // // grafo.adcAresta('B', 'E', 4, 'nao_orientado');
-  // // grafo.adcAresta('C', 'E', 5, 'nao_orientado');
-  // // grafo.adcAresta('D', 'E', 2, 'nao_orientado');
-  // // grafo.adcAresta('D', 'F', 3, 'nao_orientado');
-  // // grafo.adcAresta('E', 'F', 5, 'nao_orientado');
-  // grafo.prim('E');
-
+  const grafo = new Grafo(); // instancia grafo
   const [nodeSelect, setNodeSelect] = useState();
   const [custo, setCusto] = useState(0);
   const [edge, setEdge] = useState([]);
@@ -67,35 +17,36 @@ function Dashboard() {
   });
 
   useEffect(() => {
+    // inicializa grafo de maneira aleatoria
     var node = [];
     var edge = [];
-    var tamanho = 10; // numero de nós
+    var tamanho = 20; // numero de nós
     for (var i = 0; i < tamanho; i++) {
+      var from = Aleatorio(Aleatorio(1, tamanho - 1), tamanho - 1);
       var to = Aleatorio(1, tamanho - 1);
       var value = Aleatorio(1, 100);
       node.push({ id: i, label: `Casa ${i}` });
-      //grafo.adcVertice(`${i}`);
-
       edge.push({
-        from: i,
+        from: from,
         to: to,
         label: `${value}`,
+        id: i,
       });
-
-      //grafo.adcAresta(`${i}`, `${to}`, value);
     }
+    //=========================
+    // salva grafo  aleatorio no state
     setEdge(edge);
     setgraph({
       nodes: node,
       edges: edge,
     });
+    //==========================
   }, []);
 
   const options = {
     // define as opçoes para renderizaçao do grafo
     interaction: { multiselect: true, hoverConnectedEdges: true },
     autoResize: true,
-
     height: '100%',
     width: '100%',
     nodes: {
@@ -104,19 +55,34 @@ function Dashboard() {
       color: {
         border: '#222222',
         background: '#666666',
+        highlight: {
+          border: '#000080',
+          background: '#4682B4',
+        },
       },
       font: { color: '#eeeeee' },
     },
     edges: {
       color: 'lightgray',
+      arrows: {
+        to: {
+          enabled: false,
+        },
+        from: {
+          enabled: false,
+        },
+      },
     },
   };
   const events = {
     // função que captura os nós selecionados pelo usuario
     select: function (event) {
       var { nodes, edges } = event;
+      //=======================
+      // adiciona os nos e arestas selecionados no state
       setNodeSelect(nodes);
       setSelectEdge(edges);
+      //======================
     },
   };
 
@@ -126,32 +92,43 @@ function Dashboard() {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
+
+  // funçao que envia dados para o algoritmo de Prim e exibe os erutados em tela
   function CreateGraph() {
     var n = [];
     var e = [];
+    //  ===========================
+    // adicona os nos para o Algoritmo de Prim
     nodeSelect.map((no) => {
-      console.log(no);
       grafo.adcVertice(`${no}`);
       n.push({ id: no, label: `Casa ${no}` });
     });
+    //==============================
+
+    //==============================
+    // adiciona arestas para o Algoritmo de Prim
     edge.map((ed) => {
       var len = 0;
-      //grafo.adcVertice(`${no}`);
-
       while (len < selectEdge.length) {
         if (selectEdge[len] == ed['id']) {
-          console.log(ed);
+          // verifica dados  das arestas pelo ID
           grafo.adcAresta(
+            // adiona no grafro
             `${ed['from']}`,
             `${ed['to']}`,
-            parseInt(ed['label'], 10)
+            parseInt(ed['label'], 10),
+            'nao_orientado'
           );
         }
         len++;
       }
+      //=====================================
     });
-    grafo.prim(`${nodeSelect[0]}`);
-    var caminho = grafo.getCaminho();
+
+    grafo.prim(`${nodeSelect[0]}`); // inicia o algoritmo de prim, dizendo qual deve ser o no inical
+    var caminho = grafo.getCaminho(); // retona a lista de arestas de menor custo.
+    /// ===============================
+    // renderiza um novo grafro
     caminho.map((Cami) => {
       e.push({
         from: Cami['origem'],
@@ -164,8 +141,95 @@ function Dashboard() {
       nodes: n,
       edges: e,
     });
-    console.log(grafo.getCusto());
-    setCusto(grafo.getCusto());
+    //=================================
+    setCusto(grafo.getCusto()); // retorna o custo de percorrer determinado caminho
+  }
+
+  //===============================================
+  // funcçoes que carega exemplo da aula
+  function Exemp1() {
+    setEdge([
+      { from: 'A', to: 'B', label: '4', id: '1' },
+      { from: 'A', to: 'C', label: '6', id: '2' },
+      { from: 'A', to: 'D', label: '16', id: '3' },
+      { from: 'B', to: 'G', label: '24', id: '4' },
+      { from: 'C', to: 'E', label: '5', id: '5' },
+      { from: 'C', to: 'D', label: '8', id: '6' },
+      { from: 'C', to: 'G', label: '23', id: '7' },
+      { from: 'D', to: 'E', label: '10', id: '8' },
+      { from: 'D', to: 'H', label: '21', id: '9' },
+      { from: 'E', to: 'G', label: '18', id: '10' },
+      { from: 'E', to: 'F', label: '11', id: '11' },
+      { from: 'E', to: 'H', label: '14', id: '12' },
+      { from: 'F', to: 'G', label: '9', id: '13' },
+      { from: 'F', to: 'H', label: '7', id: '14' },
+    ]);
+
+    setgraph({
+      nodes: [
+        { id: 'A', label: 'A' },
+        { id: 'B', label: 'B' },
+        { id: 'C', label: 'C' },
+        { id: 'D', label: 'D' },
+        { id: 'E', label: 'E' },
+        { id: 'F', label: 'F' },
+        { id: 'G', label: 'G' },
+        { id: 'H', label: 'H' },
+      ],
+      edges: [
+        { from: 'A', to: 'B', label: '4', id: '1' },
+        { from: 'A', to: 'C', label: '6', id: '2' },
+        { from: 'A', to: 'D', label: '16', id: '3' },
+        { from: 'B', to: 'G', label: '24', id: '4' },
+        { from: 'C', to: 'E', label: '5', id: '5' },
+        { from: 'C', to: 'D', label: '8', id: '6' },
+        { from: 'C', to: 'G', label: '23', id: '7' },
+        { from: 'D', to: 'E', label: '10', id: '8' },
+        { from: 'D', to: 'H', label: '21', id: '9' },
+        { from: 'E', to: 'G', label: '18', id: '10' },
+        { from: 'E', to: 'F', label: '11', id: '11' },
+        { from: 'E', to: 'H', label: '14', id: '12' },
+        { from: 'F', to: 'G', label: '9', id: '13' },
+        { from: 'F', to: 'H', label: '7', id: '14' },
+      ],
+    });
+  }
+  // =======================================================
+  //Exemplo 2
+  function Exemp2() {
+    setEdge([
+      { from: 'A', to: 'B', label: '2', id: '1' },
+      { from: 'A', to: 'C', label: '3', id: '2' },
+      { from: 'B', to: 'C', label: '5', id: '3' },
+      { from: 'B', to: 'D', label: '4', id: '4' },
+      { from: 'B', to: 'E', label: '4', id: '5' },
+      { from: 'C', to: 'E', label: '5', id: '6' },
+      { from: 'D', to: 'E', label: '2', id: '7' },
+      { from: 'D', to: 'F', label: '3', id: '8' },
+      { from: 'E', to: 'F', label: '5', id: '9' },
+    ]);
+
+    setgraph({
+      nodes: [
+        { id: 'A', label: 'A' },
+        { id: 'B', label: 'B' },
+        { id: 'C', label: 'C' },
+        { id: 'D', label: 'D' },
+        { id: 'E', label: 'E' },
+        { id: 'F', label: 'F' },
+      ],
+      edges: [
+        { from: 'A', to: 'B', label: '2', id: '1' },
+        { from: 'A', to: 'C', label: '3', id: '2' },
+        { from: 'B', to: 'C', label: '5', id: '3' },
+        { from: 'B', to: 'D', label: '4', id: '4' },
+        { from: 'B', to: 'E', label: '4', id: '5' },
+        { from: 'C', to: 'E', label: '5', id: '6' },
+        { from: 'D', to: 'E', label: '2', id: '7' },
+        { from: 'D', to: 'F', label: '3', id: '8' },
+        { from: 'E', to: 'F', label: '5', id: '9' },
+      ],
+    });
   }
 
   return (
@@ -176,12 +240,16 @@ function Dashboard() {
             Selecione os nós que deseja incluir no caminho, usando Ctrl+click
           </p>
         </div>
+        <button type="submit" onClick={Exemp1}>
+          Exemplo de aula 1{' '}
+        </button>
+        <button type="submit" onClick={Exemp2}>
+          Exemplo de aula 2
+        </button>
         <button type="submit" onClick={CreateGraph}>
           Gerar Menor caminho
         </button>
-        <Form onSubmit={() => {}}>
-          <Input name="origem" type="fieldName" placeholder="Nó de Origem" />
-        </Form>
+
         <div>
           <p>
             {`Para percorrer o caminho selcionado , da melhor forma possivel, você
@@ -189,7 +257,17 @@ function Dashboard() {
           </p>
         </div>
       </Menu>
-      <Graph graph={graph} options={options} events={events} />
+      <Graph
+        graph={graph}
+        options={options}
+        events={events}
+        getNetwork={(network) => {
+          //  if you want access to vis.js network api you can set the state in a parent component using this property
+          {
+            console.log(network);
+          }
+        }}
+      />
     </Container>
   );
 }
