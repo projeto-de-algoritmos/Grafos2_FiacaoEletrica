e87d491447e95508a41991d69c491f2c570332ee
Andre Eduardo <andre.eduardo11@hotmail.com>
2020-10-02 21:04:58 -0300

    Updates

---
+++ precache-manifest.3fb1423536f0bf4a3a00124f284ade05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb213cef1e1066ba12bfae3c009bdfba",
+    "url": "/Grafos2_FiacaoEletrica/index.html"
+  },
+  {
+    "revision": "53a6e404db4378960f96",
+    "url": "/Grafos2_FiacaoEletrica/static/css/2.f45fb83f.chunk.css"
+  },
+  {
+    "revision": "53a6e404db4378960f96",
+    "url": "/Grafos2_FiacaoEletrica/static/js/2.88f6dd2d.chunk.js"
+  },
+  {
+    "revision": "5ce43066b62c06c3dc78b6f331d6696a",
+    "url": "/Grafos2_FiacaoEletrica/static/js/2.88f6dd2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64ac0a5f688c23e806f4",
+    "url": "/Grafos2_FiacaoEletrica/static/js/main.bf1b2d58.chunk.js"
+  },
+  {
+    "revision": "5a08731e070dc1c5c530",
+    "url": "/Grafos2_FiacaoEletrica/static/js/runtime-main.e8fb4a1d.js"
+  },
+  {
+    "revision": "bbc62c8308c0c0d0f156f71fa130831e",
+    "url": "/Grafos2_FiacaoEletrica/static/media/torre.bbc62c83.png"
+  }
+]);
